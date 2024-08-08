451cb72efb1fadadfc9f7a5ba30b8ecd44bfb7f0
maciej.lyszczarz <maciej.lyszczarz@jamf.com>
2024-08-09 01:06:06 +0200

    19

---
+++ src/19/index.test.ts
@@ -0,0 +1,32 @@
+import { describe, expect, test } from "vitest";
+import testing from ".";
+import { arrToList } from "../utils/ListNode";
+
+describe("19", () => {
+    test("should pass", () => {
+        const input = [1, 2, 3, 4, 5],
+            n = 2;
+        const output = [1, 2, 3, 5];
+
+        const result = testing(arrToList(input), n);
+
+        expect(result).toStrictEqual(arrToList(output));
+    });
+    test("should pass", () => {
+        const input = [1],
+            n = 1;
+
+        const result = testing(arrToList(input), n);
+
+        expect(result).toStrictEqual(null);
+    });
+    test("should pass", () => {
+        const input = [1, 2],
+            n = 1;
+        const output = [1];
+
+        const result = testing(arrToList(input), n);
+
+        expect(result).toStrictEqual(arrToList(output));
+    });
+});
---
+++ src/19/index.ts
@@ -0,0 +1,9 @@
+import { ListNode } from "../utils/ListNode";
+
+export default function removeNthFromEnd(
+  head: ListNode | null,
+  n: number,
+): ListNode | null {
+  console.log("adw");
+  return null;
+}
