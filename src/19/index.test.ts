1a8390c3319cdae4f2ea7b42f78b6179154efe18
maciej.lyszczarz <maciej.lyszczarz@jamf.com>
2024-08-09 02:01:49 +0200

    19

--- src/19/index.test.ts
+++ src/19/index.test.ts
@@ -3,30 +3,39 @@ import testing from ".";
 import { arrToList } from "../utils/ListNode";
 
 describe("19", () => {
-    test("should pass", () => {
-        const input = [1, 2, 3, 4, 5],
-            n = 2;
-        const output = [1, 2, 3, 5];
+  test("should pass", () => {
+    const input = [1, 2, 3, 4, 5],
+      n = 2;
+    const output = [1, 2, 3, 5];
 
-        const result = testing(arrToList(input), n);
+    const result = testing(arrToList(input), n);
 
-        expect(result).toStrictEqual(arrToList(output));
-    });
-    test("should pass", () => {
-        const input = [1],
-            n = 1;
+    expect(result).toStrictEqual(arrToList(output));
+  });
+  test("should pass", () => {
+    const input = [1],
+      n = 1;
 
-        const result = testing(arrToList(input), n);
+    const result = testing(arrToList(input), n);
 
-        expect(result).toStrictEqual(null);
-    });
-    test("should pass", () => {
-        const input = [1, 2],
-            n = 1;
-        const output = [1];
+    expect(result).toStrictEqual(null);
+  });
+  test("should pass", () => {
+    const input = [1, 2],
+      n = 1;
+    const output = [1];
 
-        const result = testing(arrToList(input), n);
+    const result = testing(arrToList(input), n);
 
-        expect(result).toStrictEqual(arrToList(output));
-    });
+    expect(result).toStrictEqual(arrToList(output));
+  });
+  test("should pass", () => {
+    const input = [1, 2],
+      n = 2;
+    const output = [2];
+
+    const result = testing(arrToList(input), n);
+
+    expect(result).toStrictEqual(arrToList(output));
+  });
 });
--- src/19/index.ts
+++ src/19/index.ts
@@ -4,6 +4,26 @@ export default function removeNthFromEnd(
   head: ListNode | null,
   n: number,
 ): ListNode | null {
-  console.log("adw");
-  return null;
+  let [first, curr, last] = [head, head, head];
+  let deletePos = -n;
+  while (last) {
+    last = last.next;
+    deletePos++;
+  }
+
+  for (let i = 0; i < deletePos - 1; i++) {
+    curr = curr!.next;
+  }
+
+  if (deletePos === 0 && first) {
+    return first.next;
+  }
+
+  if (curr && curr.next) {
+    curr.next = curr.next.next || null;
+  } else {
+    return null;
+  }
+
+  return first;
 }
