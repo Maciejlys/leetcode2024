d99337573c78ea0798499dbfb3dbcd681d39e578
maciej.lyszczarz <maciej.lyszczarz@jamf.com>
2024-08-10 04:15:15 +0200

    287

--- src/287/index.ts
+++ src/287/index.ts
@@ -1,3 +1,17 @@
 export default function findDuplicate(nums: number[]): number {
-  return 0;
+  let [slow, fast] = [0, 0];
+
+  do {
+    slow = nums[slow];
+    fast = nums[nums[fast]];
+  } while (slow != fast);
+
+  let slow2 = 0;
+
+  do {
+    slow = nums[slow];
+    slow2 = nums[slow2];
+  } while (slow != slow2);
+
+  return slow;
 }
