67c45fffb87f968c0d2d42701bcd94779824f276
maciej.lyszczarz <maciej.lyszczarz@jamf.com>
2024-08-10 15:32:28 +0200

    test fix

--- src/287/index.test.ts
+++ src/287/index.test.ts
@@ -3,25 +3,24 @@ import testing from ".";
 import { TestCases } from "../types/testCases";
 
 describe("287", () => {
-    const tests: TestCases<[number[]], number> = [
-        {
-            args: [[1, 3, 4, 2, 2]],
-            want: 2,
-        },
-        {
-            args: [[3, 1, 3, 4, 2]],
-            want: 3,
-        },
-        {
-            args: [[3, 3, 3, 3, 3]],
-            want: 3,
-        },
-    ];
+  const tests: TestCases<[number[]], number> = [
+    {
+      args: [[1, 3, 4, 2, 2]],
+      want: 2,
+    },
+    {
+      args: [[3, 1, 3, 4, 2]],
+      want: 3,
+    },
+    {
+      args: [[3, 3, 3, 3, 3]],
+      want: 3,
+    },
+  ];
 
-    for (let t of tests) {
-        test("should pass", () => {
-            const result = testing(...t.args);
-            expect(result).toStrictEqual(t.want);
-        });
-    }
+  for (let t of tests) {
+    test("should pass", () => {
+      expect(testing(...t.args)).toStrictEqual(t.want);
+    });
+  }
 });
